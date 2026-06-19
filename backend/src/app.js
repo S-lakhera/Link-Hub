5ec03c2a971e166ee9886a547eae43ae3bec2187
Shashank Lakhera <lakherashashank70@gmail.com>
2026-06-20 00:17:05 +0530

    Add user model and implement password hashing; update dependencies in package.json and package-lock.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 const app = express();
 
-app.get("/", (req,res) => {
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials:true,
+}))
+
+
+app.get("/", (req, res) => {
     res.send("Healthy connection")
 })
 
